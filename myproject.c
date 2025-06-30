e0a2db44a70df13ec7e60e8bdd6956c2d48e1889
Ifeanyi Amah <amahifeanyi@gmail.com>
2025-06-30 09:11:41 +0100

    capstone project

---
+++ myproject.c
@@ -0,0 +1,49 @@
+// #include "include/input_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    srand(time(0));
+
+    int endGuess, guess, attempts = 0; 
+
+    int randomNumber = (rand() % 50) + 1;
+
+    // printf("Random Number: %d\n", randomNumber);
+
+    while (endGuess == 0){
+        // int guess = get_int("Guess the Number (1 to 50)");
+        printf("Guess the Number (1 to 50): ");
+        scanf("%d", &guess); // &guess passes the address of 'guess'
+        attempts = attempts + 1;
+        if (randomNumber > guess){
+            printf("Too low! Try again! \n");
+        }else if (randomNumber < guess){
+            printf("Too High! Try again! \n");
+        }else{
+            if (attempts > 1){
+                printf("Correct! You guessed it in %d attempts.\n", attempts);
+            }else{
+                printf("Correct! You guessed it in %d attempt.\n", attempts);
+            }
+            endGuess = 1;
+            // randomNumber = (rand() % 50) + 1;
+            // attempts = 0;
+            // printf("Random Number: %d\n", randomNumber);
+        }
+    }
+
+    
+
+    // printf("hello world \n");
+
+    return 0;
+
+
+
+}
+
+
+
